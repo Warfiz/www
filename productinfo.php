fc565e9b31f330b6fd41a5e0867db9c270d31701
ErikUu <erik.uusitalo@gmail.com>
2015-12-02 22:10:34 +0100

    minor fix

--- productinfo.php
+++ productinfo.php
@@ -1,10 +1,48 @@
 <!DOCTYPE html>
+<?php
+  session_start();
+  if(!isset($_SESSION['authenticated'])) {
+    header( "Location: index.php" );
+  };
+
+  include 'inc/getProductInfo';
+?>
+
 <html>
+
   <head>
+
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>MeatMarket</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css" charset="utf-8">
+    <link rel="stylesheet" href="css/products.css" charset="utf-8">
+
   </head>
+
   <body>
-    <h1>This is info for product <?php echo $_GET['pid'];?></h1>
+
+    <div class="sidebar">
+      <div class="navigation-links">
+        <ul>
+          <li><a href="account.php">Account</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="basket.php">Basket</a></li>
+          <li><a href="reviews.php">Reviews</a></li>
+          <li><a href="php/logout.php">Log out</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="wrapper">
+
+
+
+    </div>
+
+
   </body>
 </html>
